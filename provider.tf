0af82f466177524c9bd1058a5f4c1c66941db42c
santosh2kumar <santosh2kumar@gmail.com>
2020-03-04 09:07:56 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,6 +1,6 @@
 variable "region" {
   description = "The region to create your two VPCs in, such as `us-south`. The VPCs are created in two separate zones within the same region. To get a list of all regions, run `ibmcloud is regions`."
-  default = "us-south"
+  default = "Dallas"
 }
 
 
